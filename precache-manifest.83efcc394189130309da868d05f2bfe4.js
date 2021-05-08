1c8ccb09ac709089a7c957e5c758104a59c42486
raptazure <hermit0x9@outlook.com>
2021-05-08 19:28:25 +0800

    Updates

---
+++ precache-manifest.83efcc394189130309da868d05f2bfe4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d7c584c36f6fda62380cfe6730694f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af52bd8740d277991816",
+    "url": "/static/css/main.3fc8841a.chunk.css"
+  },
+  {
+    "revision": "a4a2ed83cd7b2509ab8b",
+    "url": "/static/js/2.5e7984bf.chunk.js"
+  },
+  {
+    "revision": "95dac241b46b34feae5bea722f7e59af",
+    "url": "/static/js/2.5e7984bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af52bd8740d277991816",
+    "url": "/static/js/main.b2fb8e7b.chunk.js"
+  },
+  {
+    "revision": "2ef8687fccace545346f",
+    "url": "/static/js/runtime-main.003cf03d.js"
+  }
+]);
