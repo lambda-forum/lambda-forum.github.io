438e572d18ffeace1cce1e68c397ea24bf1bf6cb
raptazure <hermit0x9@outlook.com>
2021-05-08 19:24:11 +0800

    init

---
+++ src/config.js
@@ -0,0 +1,33 @@
+export const config = {
+  // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
+  githubConvertedToken:
+    "Z2hwX2did056cmFHTEthSFFMSDBXbHZzOHM5MmQ5dHE0eTBkSGsxRQ==",
+
+  // Your Github UserName
+  githubUserName: "lambda-forum",
+
+  // Your Github Repo Name Where You Have your issues as Blog
+  githubRepo: "lambda-forum",
+
+  // Set it to true if you have a Github app to add to this project
+  // and fill the client ID & secret
+  enableOAuth: true,
+  OAuthClientID: "Iv1.aa33500a7908669b",
+  OAuthSecret: "8e2d3524a81ba511439c8c1551671cfab373af01",
+
+  // Your Personal Blog Title
+  title: "λ",
+
+  // Your Personal Blog Subtitle
+  subtitle: "Developer",
+
+  // Header customization
+  header: {
+    backgroundColor: "#f1f6f8", // can be a CSS gradient
+    backgroundColorDark: "#4C566A",
+    titleColor: "#ff5252",
+    titleColorDark: "#16a085",
+    subtitleColor: "#37474f",
+    subtitleColorDark: "#D8DEE9",
+  },
+};
